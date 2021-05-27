3e30f12398a21bba48b8eae2023540a0ba2b4c53
Maxlego08 <maxlego08@sfr.fr>
2021-05-27 16:04:27 +0200

    :construction: Ajout de la redirection vers la page location

--- src/app/locations/locations.component.ts
+++ src/app/locations/locations.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {VehicleService} from '../vehicle.service';
 import {IVehicle} from '../vehiclee';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'app-locations',
@@ -12,15 +13,16 @@ export class LocationsComponent implements OnInit {
   public vehicles = [];
 
   // tslint:disable-next-line:variable-name
-  constructor(private _vehicleService: VehicleService) {
+  constructor(private _vehicleService: VehicleService, private router: Router, private route: ActivatedRoute) {
   }
 
   ngOnInit(): void {
     this._vehicleService.getVehicles().subscribe(data => this.vehicles = data);
   }
 
-  onVehicleClick(vehicle: IVehicle){
-    console.log(vehicle);
+  onVehicleClick(vehicle: IVehicle) {
+    const vehicleId = vehicle ? vehicle.id : null;
+    this.router.navigate(['/locations', vehicleId], {relativeTo: this.route});
   }
 
 }
