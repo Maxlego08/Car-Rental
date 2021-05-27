46d2580f9aa7ecef074d00e528b9a5b2ab7bf3bc
Maxlego08 <maxlego08@sfr.fr>
2021-05-27 14:10:29 +0200

    :construction: Création d'un componant pour la page home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { LocationsComponent } from './locations/locations.component';
 import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LocationsComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
