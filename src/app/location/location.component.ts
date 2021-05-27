03e80f42c76262670a991df895165534ad9018a8
Maxlego08 <maxlego08@sfr.fr>
2021-05-27 16:15:46 +0200

    :construction: Ajout de la récupération du véhicule

--- src/app/location/location.component.ts
+++ src/app/location/location.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {VehicleService} from '../vehicle.service';
+import {IVehicle} from '../vehiclee';
 
 @Component({
   selector: 'app-location',
@@ -7,9 +10,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LocationComponent implements OnInit {
 
-  constructor() { }
+  public vehicle: IVehicle;
+
+  constructor(private route: ActivatedRoute, private vehicleService: VehicleService) {
+  }
 
   ngOnInit(): void {
+    // tslint:disable-next-line:radix
+    const id = parseInt(this.route.snapshot.paramMap.get('id'));
+    this.vehicleService.getVehicles().subscribe(data => {
+      data.forEach(vehicle => {
+        if (vehicle.id === id) {
+          this.vehicle = vehicle;
+        }
+      });
+    });
   }
 
 }
