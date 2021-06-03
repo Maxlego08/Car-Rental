8c76a0551c515c4cba9ab76abeac292873398ba7
Maxlego08 <maxlego08@sfr.fr>
2021-06-03 15:05:05 +0200

    :bug: Correction app routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LocationsComponent } from './locations/locations.component';
+import { LocationComponent } from './location/location.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { ContactComponent } from './contact/contact.component';
@@ -8,7 +9,7 @@ import { ContactComponent } from './contact/contact.component';
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'locations', component: LocationsComponent },
-  {path: 'locations/:id', component: LocationsComponent},
+  {path: 'locations/:id', component: LocationComponent},
   { path: 'login', component: LoginComponent },
   { path: 'contact', component: ContactComponent }
 ];
